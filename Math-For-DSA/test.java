8f7667288db73c871850407ed4f94d86affef794
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2026-09-09 02:02:54 +0530

    Employee Management System: Multiple Employee using arraylist

---
+++ Math-For-DSA/test.java
@@ -0,0 +1,5 @@
+package Math-For-DSA;
+
+public class test {
+    
+}
--- Project/EmployeeManagementSystem.java
+++ Project/EmployeeManagementSystem.java
@@ -1,8 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class EmployeeManagementSystem {
     static Scanner sc = new Scanner(System.in);
-    static Employee e1;
+    static ArrayList<Employee> e1 = new ArrayList<>();
     public static void main(String[] args) {
 
         System.out.println();
@@ -25,35 +26,56 @@ public class EmployeeManagementSystem {
             inp = sc.nextInt();
             System.out.println();
 
+
+
             int id = 0;
             String name = null;
             int age = 0;
             String department = null;
             double salary = 0.0;
 
-            
-
             switch (inp) {
+                
                 case 1:
                     employeeDetails(id, name, age, department, salary);
                     break;
                 case 2:
-                    e1.display(); //2. displaying employee details
-                    System.out.println();
+                    displayEmployee();
                     break;
                 case 3:
                     System.out.print("Enter employee id: ");
                     int checkId = sc.nextInt();
-                    searchEmployee(checkId);
+                    int foundEmployee = employeeFound(checkId);
+                    if(foundEmployee != -1) {
+                        searchEmployee(foundEmployee);
+                    } else {
+                        System.out.println("Employee not found");
+                        System.out.println();
+                    }
+                    
                     break;
                 case 4:
-                    updateEmployeeInfo(id, name, age, department, salary);
+                    System.out.print("Enter employee id: ");
+                    int updateId = sc.nextInt();
+                    int employeeFound = employeeFound(updateId);
+                    if(employeeFound != -1) {
+                        updateEmployeeInfo(id, name, age, department, salary, employeeFound);
+                    } else {
+                        System.out.println("Employee not found");
+                        System.out.println();
+                    }
                     break;
                 case 5:
                     int deleteId;
                     System.out.print("Enter employee id to delete details");
                     deleteId = sc.nextInt();
-                    deleteEmployeeInfo(deleteId);
+                    int employeeDelete = employeeFound(deleteId);
+                    if(employeeDelete != -1) {
+                        deleteEmployeeInfo(employeeDelete);
+                    } else {
+                        System.out.println("Employee does'nt exist");
+                        System.out.println();
+                    }
                     break;
                 case 6: 
                     System.out.println("Thanks for visiting!!");
@@ -79,52 +101,52 @@ public class EmployeeManagementSystem {
         department = sc.nextLine();
         System.out.print("Enter salary: ");
         salary = sc.nextDouble();
-        e1 = new Employee(id, name, age, department, salary);
+        e1.add(new Employee(id, name, age, department, salary));
         System.out.println("Employee Details addded successfully");
         System.out.println();
     }
-    //3. Search an Employee
-    static void searchEmployee(int checkId) {
-        if(checkId == e1.getId()) {
-            System.out.println("Employee Found!!");
-            System.out.println("Employee name: " + e1.getName());
-            System.out.println();
-        } else {
-            System.out.println("Employee not found");
-            System.out.println();
+    //2. Display employee
+    static void displayEmployee() {
+        for (int i = 0; i < e1.size(); i++) {
+            e1.get(i).display();
         }
     }
+    //3. Search an Employee
+    static void searchEmployee(int checkId) {
+            System.out.println("Employee Found!!");
+            System.out.println("Employee ID: " + e1.get(checkId).getId()+ ", Employee name: " + e1.get(checkId).getName());
+            System.out.println();
+        }
     //4. Updating Employee Info
-    static void updateEmployeeInfo(int id, String name, int age, String department, double salary) {
-        System.out.print("Enter employee id: ");
-        e1.setId(sc.nextInt());
-        sc.nextLine();
-        System.out.print("Enter employee name: ");
-        e1.setName(sc.nextLine());
-        System.out.print("Enter employee age: ");
-        e1.setAge(sc.nextInt());
-        sc.nextLine();
-        System.out.print("Enter employee department: ");
-        e1.setDepartment(sc.nextLine());
-        System.out.print("Enter employee salary: ");
-        e1.setSalary(sc.nextDouble());
-        System.out.println("Employee details updated successfully");
-        System.out.println();
+    static void updateEmployeeInfo(int id, String name, int age, String department, double salary, int updateId) {
+            System.out.print("Enter employee id: ");
+            e1.get(updateId).setId(sc.nextInt());
+            sc.nextLine();
+            System.out.print("Enter employee name: ");
+            e1.get(updateId).setName(sc.nextLine());
+            System.out.print("Enter employee age: ");
+            e1.get(updateId).setAge(sc.nextInt());
+            sc.nextLine();
+            System.out.print("Enter employee department: ");
+            e1.get(updateId).setDepartment(sc.nextLine());
+            System.out.print("Enter employee salary: ");
+            e1.get(updateId).setSalary(sc.nextDouble());
+            System.out.println("Employee details updated successfully");
+            System.out.println();
+
     }
     // 5. Delete employee details
     static void deleteEmployeeInfo(int deleteId) {
-        if(deleteId == e1.getId()) {
-            e1.setId(0);
-            e1.setName(null);
-            e1.setAge(0);
-            e1.setDepartment(null);
-            e1.setSalary(0.0);
-            System.out.println("Employee details deleted successfully");
-            System.out.println();
-        } else {
-            System.out.println("This employee id does'nt exist");
-            System.out.println();
+        e1.remove(deleteId);
+    }
+    //Helper function ==> Checks whether employee exists or not
+    static int employeeFound(int updateId) {
+        for (int i = 0; i < e1.size(); i++) {
+            if(updateId == e1.get(i).getId()) {
+                return i;
+            }
         }
+        return -1;
     }
 }
 
@@ -188,10 +210,6 @@ class Employee {
     }
 
     public void display() {
-        System.out.println("Employee ID: " + this.id);
-        System.out.println("Name: " + this.name);
-        System.out.println("Age: " + this.age);
-        System.out.println("Department: " + this.department);
-        System.out.println("Salary: " + this.salary);
+        System.out.println("Employee ID: " + this.id + ", Name: " + this.name + ", Age: " + this.age + ", Department: " + this.department + ", Salary: " + this.salary);
     }
 }
