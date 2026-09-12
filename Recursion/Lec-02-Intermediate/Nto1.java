41ca3ef0a9d47e19d863661d36b017ac7b9d0126
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2026-09-12 10:26:27 +0530

    nto1

---
+++ Recursion/Lec-02-Intermediate/Nto1.java
@@ -0,0 +1,13 @@
+
+public class Nto1 {
+    public static void main(String[] args) {
+        print(10);
+    }
+    static void print(int n) {
+        if(n == 0) {
+            return;
+        }
+        System.out.println(n);
+        print(n - 1);
+    }
+}
