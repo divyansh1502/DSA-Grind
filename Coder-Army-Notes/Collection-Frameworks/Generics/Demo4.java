1e8f692fc5a5eec801c9f8cde2cbe1c0b2125ea8
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2026-09-13 15:04:49 +0530

    Passing multiple datatupes inside generic type parameter

---
+++ Coder-Army-Notes/Collection-Frameworks/Generics/Demo4.java
@@ -0,0 +1,17 @@
+
+public class Demo4 {
+    public static void main(String[] args) {
+        Pair<String, Integer> p = new Pair<>("Divyansh Singh", 21);
+        
+        System.out.println(p.first + ": " + p.second);
+    }
+}
+class Pair<T, U> {
+    T first;
+    U second;
+
+    Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+}
