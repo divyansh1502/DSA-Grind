0411afda96e0e15a0b563a416c89b518dc72c7c7
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2026-09-15 20:58:05 +0530

    BMS Existing User Completed

--- Project/Bank-Management-System/ExistingCustomer.java
+++ Project/Bank-Management-System/ExistingCustomer.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ExistingCustomer {
@@ -7,7 +8,7 @@ public class ExistingCustomer {
     static private String ifsc = "CBIN07890";
     static private String branch = "Sarojini Nagar, Lucknow";
     static private String status = "Active";
-
+    static private double balance = 0;
     static Scanner sc = new Scanner(System.in);
     public static void coustomerLogin() {
         System.out.println();
@@ -92,18 +93,94 @@ public class ExistingCustomer {
         System.out.println();
     }
     private static void checkBalance() {
-
+        System.out.println();
+        System.out.println("==================Balance=================");
+        System.out.println();
+        System.out.println("Balance: " + balance);
+        System.out.println();
     }
     private static void depositMoney() {
-
+        System.out.println();
+        System.out.println("==================Deposit Money=================");
+        System.out.println();
+        System.out.print("Enter Amount: ");
+        double amt = sc.nextDouble();
+        balance += amt;
+        Transaction transaction = new Transaction("Deposit", amt, balance);
+        Transaction.transactions.add(transaction);
+        System.out.println("Money Deposited Successfully!");
+        System.out.println("Current Balance: " + balance);
+        System.out.println();
     }
     private static void withdrawMoney() {
-
+        System.out.println();
+        System.out.println("==================Withdraw Money=================");
+        System.out.println();
+        System.out.print("Enter Amount: ");
+        double amt = sc.nextDouble();
+        if(balance >= amt) {
+            balance -= amt;
+            Transaction transaction = new Transaction("Withdraw", amt, balance);
+            Transaction.transactions.add(transaction);
+            System.out.println("Money Withdrawn Successfully!");
+            System.out.println();
+        } else {
+            System.out.println("Low Balance!");
+            System.out.println();
+        }
+        System.out.println("Current Balance: " + balance);
+        System.out.println();
     }
     private static void transferMoney() {
-
+        System.out.println();
+        System.out.println("==================transfer Money=================");
+        System.out.println();
+        System.out.print("Enter Account Number: ");
+        long acc = sc.nextLong();
+        System.out.print("Enter Amount: ");
+        double amt = sc.nextDouble();
+        if(balance >= amt) {
+            balance -= amt;
+            Transaction transaction = new Transaction("Transfer", amt, balance);
+            Transaction.transactions.add(transaction);
+            System.out.println("Money Transferred Successfully!");
+            System.out.println("Account Number: " + acc);
+            System.out.println("Transferred Money: " + amt);
+            System.out.println();
+        } else {
+            System.out.println("Low Balance!");
+            System.out.println();
+        }
+        System.out.println("Current Balance: " + balance);
+        System.out.println();
     }
     private static void transactionHistory() {
-
+        System.out.println();
+        System.out.println("=================Transaction History==============");
+        System.out.println();
+        if(Transaction.transactions.isEmpty()) {
+            System.out.println("No transaction yet");
+        } else {
+            for (Transaction t : Transaction.transactions) {
+                System.out.println();
+                System.out.println("Type: " + t.type);
+                System.out.println("Amount: " + t.amount);
+                System.out.println("Balance After: " + t.balanceAfter);
+                System.out.println();
+            }
+        }
     }
+}
+class Transaction {
+    String type;
+    double amount;
+    double balanceAfter;
+
+    public Transaction(String type, double amount, double balanceAfter) {
+        this.type = type;
+        this.amount = amount;
+        this.balanceAfter = balanceAfter;
+    }
+    static ArrayList<Transaction> transactions = new ArrayList<>();
+    
 }
