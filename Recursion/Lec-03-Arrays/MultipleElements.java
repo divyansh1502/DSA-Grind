33a4f0dff13e67a58aec8f58fde4896cfa9ff3ed
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2026-09-15 10:21:57 +0530

    Return list

--- Recursion/Lec-03-Arrays/MultipleElements.java
+++ Recursion/Lec-03-Arrays/MultipleElements.java
@@ -5,6 +5,7 @@ public class MultipleElements {
         int[] arr = {1, 2, 4, 4, 4, 6, 8, 11, 25, 34, 36};
         multipleElements(arr, 4, 0);
         System.out.println(list);
+        System.out.println(returnList(arr, 4, 0, list));
     }
     static ArrayList<Integer> list = new ArrayList<>();
     public static int multipleElements(int[] arr, int target, int index) {
@@ -15,5 +16,14 @@ public class MultipleElements {
             list.add(index);
         }
         return multipleElements(arr, target,  index + 1);
+    }
+    public static ArrayList returnList(int[] arr, int target, int index, ArrayList<Integer> list) {
+        if(index == arr.length) {
+            return list;
+        } 
+        if(arr[index] == target) {
+            list.add(index);
+        }
+        return returnList(arr, target,  index + 1, list);
     }
 }
