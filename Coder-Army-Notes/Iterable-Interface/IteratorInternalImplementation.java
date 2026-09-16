95ebec14263f3c541bff5f941e7b6547fa87b778
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2026-09-17 00:09:33 +0530

    Implemting iterable interface in our own class

---
+++ Coder-Army-Notes/Iterable-Interface/IteratorInternalImplementation.java
@@ -0,0 +1,39 @@
+import java.util.Iterator;
+
+import javax.naming.Name;
+
+public class IteratorInternalImplementation {
+    public static void main(String[] args) {
+        String[] names = {"Divyansh", "Anushika", "Sudhir", "Shagun", "Shekhar"};
+        NameContainer container = new NameContainer(names);
+
+        Iterator<String> it = container.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
+class NameContainer implements Iterable<String> {
+    private String[] names;
+    private int size;
+
+    NameContainer(String[] names) {
+        this.names = names;
+        this.size = names.length;
+    }
+    @Override
+    public Iterator<String> iterator() {
+        return new NameContainerIterator();
+    }
+    private class NameContainerIterator implements Iterator<String> {
+        private int pos = 0;
+        @Override
+        public boolean hasNext() {
+            return pos < size;
+        }
+        @Override
+        public String next() {
+            return names[pos++];
+        }
+    }
+}
