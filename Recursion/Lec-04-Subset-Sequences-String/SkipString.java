8c7905b68e6f683294e3640895c8d94ed6fe0a0e
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2026-09-18 18:41:16 +0530

    Skip String

---
+++ Recursion/Lec-04-Subset-Sequences-String/SkipString.java
@@ -0,0 +1,16 @@
+
+public class SkipString {
+    public static void main(String[] args) {
+        System.out.println(skipString("ajdjapplenbzxcx"));
+    }
+    static String skipString(String str) {
+        if(str.isEmpty()) {
+            return "";
+        }
+        if(str.startsWith("apple")) {
+            return skipString(str.substring(5));
+        } else {
+            return str.charAt(0) + skipString(str.substring(1));
+        }
+    }
+}
